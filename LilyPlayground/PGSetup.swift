86fe9367730c92ba18f5965ce23539fd36055c20
Kengo Watanabe <kengo@wdkk.co.jp>
2020-12-09 18:41:01 +0900

    LilyPlaygroundHelperの廃止(extensionによる制御へ変更)

--- LilyPlayground/PGSetup.swift
+++ LilyPlayground/PGSetup.swift
@@ -19,26 +19,4 @@ public func setupPG() -> PGViewController {
     return vc
 }
 
-public var clearColor:LLColor {
-    get { PGViewController.shared.clearColor }
-    set { PGViewController.shared.clearColor = newValue }
-}
-
-public var coordRegion:LLRegion { PGViewController.shared.coordRegion }
-public var coordMinX:LLDouble { PGViewController.shared.coordMinX }
-public var coordMinY:LLDouble { PGViewController.shared.coordMinY }
-public var coordMaxX:LLDouble { PGViewController.shared.coordMaxX }
-public var coordMaxY:LLDouble { PGViewController.shared.coordMaxY }
-
-public var screenSize:LLSizeFloat { PGViewController.shared.screenSize }
-public var screenWidth:LLDouble { PGViewController.shared.width }
-public var screenHeight:LLDouble { PGViewController.shared.height }
-
-public var touches:[LBTouch] { return PGViewController.shared.touches }
-public var releases:[LBTouch] { return PGViewController.shared.releases }
-
-public var shapes:Set<LBActor> { PGViewController.shared.shapes }
-
-public var elapsedTime:Double { PGViewController.shared.elapsedTime }
-
 #endif
--- projects/apple/LilyiOSApp/codes/MyViewController.swift
+++ projects/apple/LilyiOSApp/codes/MyViewController.swift
@@ -9,6 +9,7 @@
 //
 
 import Foundation
+
 import LilySwift
 
 class MyViewController : LBViewController
