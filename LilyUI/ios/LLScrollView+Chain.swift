a057ecadaf2053a1f6325df5f33ec6dbaf44d9c5
Kengo Watanabe <kengo@wdkk.co.jp>
2021-07-19 19:10:26 +0900

    LLScrollViewのchain拡張の追加

---
+++ LilyUI/ios/LLScrollView+Chain.swift
@@ -0,0 +1,65 @@
+//
+// LilySwift Library Project
+//
+// Copyright (c) Watanabe-Denki Inc. and Kengo Watanabe.
+//   https://wdkk.co.jp/
+//
+// This software is released under the MIT License.
+//   https://opensource.org/licenses/mit-license.php
+//
+
+#if os(iOS)
+
+import UIKit
+
+/// LLScrollViewチェインアクセサ : イベント
+public extension LLChain where TObj:LLScrollView
+{
+    // MARK: -
+    
+    var setup:LLFieldMapChain<TObj, LLViewFieldMap> {
+        return LLFieldMapChain( obj, obj.setupField )
+    }
+    
+    var buildup:LLFieldMapChain<TObj, LLViewFieldMap> {
+        return LLFieldMapChain( obj, obj.buildupField )
+    }
+
+    var teardown:LLFieldMapChain<TObj, LLViewFieldMap> {
+        return LLFieldMapChain( obj, obj.teardownField )
+    }
+    
+    // MARK: -
+    
+    var defaultBuildup:LLFieldMapChain<TObj, LLViewFieldMap> {
+        return LLFieldMapChain( obj, obj.defaultBuildupField )
+    }
+    
+    var staticBuildup:LLFieldMapChain<TObj, LLViewFieldMap> {
+        return LLFieldMapChain( obj, obj.staticBuildupField )
+    }
+    
+    // MARK: -
+    
+    var touchesBegan:LLFieldMapChain<TObj, LLTouchFieldMap> {
+        return LLFieldMapChain( obj, obj.touchesBeganField ) 
+    }
+    
+    var touchesMoved:LLFieldMapChain<TObj, LLTouchFieldMap> {
+        return LLFieldMapChain( obj, obj.touchesMovedField ) 
+    }
+
+    var touchesEnded:LLFieldMapChain<TObj, LLTouchFieldMap> {
+        return LLFieldMapChain( obj, obj.touchesEndedField ) 
+    }
+    
+    var touchesEndedInside:LLFieldMapChain<TObj, LLTouchFieldMap> {
+        return LLFieldMapChain( obj, obj.touchesEndedInsideField ) 
+    }
+    
+    var touchesCancelled:LLFieldMapChain<TObj, LLTouchFieldMap> {
+        return LLFieldMapChain( obj, obj.touchesCancelledField ) 
+    }
+}
+
+#endif
