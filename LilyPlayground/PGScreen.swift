dc2e4284eb5f1286b84549aa37852a1135cbb1d4
Kengo Watanabe <kengo@wdkk.co.jp>
2021-07-27 14:54:02 +0900

    PGScreenにlatestTouchを追加

--- LilyPlayground/PGScreen.swift
+++ LilyPlayground/PGScreen.swift
@@ -18,11 +18,13 @@ public class PGScreen
 {
     public static var current:PGViewController!
     
+    // MARK: - 読み書きプロパティ
     public static var clearColor:LLColor {
         get { current.clearColor }
         set { current.clearColor = newValue }
     }
-
+    
+    // MARK: - 読み込み専用プロパティ
     public static var randomPoint:LLPoint { current.coordRegion.randomPoint }
     public static var minX:LLDouble { current.coordMinX }
     public static var minY:LLDouble { current.coordMinY }
@@ -33,10 +35,29 @@ public class PGScreen
     public static var width:LLDouble { current.width }
     public static var height:LLDouble { current.height }
 
-    public static var touches:[LBTouch] { return current.touches }
-    public static var releases:[LBTouch] { return current.releases }
+    public static var touches:[LBTouch] { current.touches }
+    public static var releases:[LBTouch] { current.releases }
 
     public static var shapes:Set<LBActor> { current.shapes }
 
     public static var elapsedTime:Double { current.elapsedTime }
+    
+    // MARK: - タッチ情報ヘルパ
+    private static var latest_touch:LBTouch = 
+        LBTouch( xy:.zero, uv: .zero, state:.touch )
+    
+    public static var latestTouch:LBTouch {
+        if let touch = PGScreen.touches.first {
+            latest_touch = touch
+        }
+        return latest_touch
+    }
+    
+    // MARK: - ユーティリティ
+    
+    // 全ての形状を削除する
+    public static func clearAllShapes() {
+        PGMemoryPool.shared.panels.removeAll()
+        PGMemoryPool.shared.triangles.removeAll()        
+    }
 }
