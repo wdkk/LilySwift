86073f8aed43dc185b17579523ae5415bde2f9a3
Kengo Watanabe <kengo@wdkk.co.jp>
2024-03-28 16:27:21 +0900

    PGShader***, BBShader***がシミュレータで動かないのでコンソールにその旨が表示されるように処理追加

--- Sources/Lily_Stage/Playground/Billboard/Accessors/Lily.Stage.Playground.BBShaderActor.swift
+++ Sources/Lily_Stage/Playground/Billboard/Accessors/Lily.Stage.Playground.BBShaderActor.swift
@@ -15,7 +15,13 @@ extension Lily.Stage.Playground.Billboard
 {   
     open class BBShaderActor : BBActor
     {        
+        static private(set) var logged = false
+        
         public init( storage:BBStorage?, shaderName:String ) {
+            #if targetEnvironment(simulator)
+            if Self.logged == false { LLLog( "BBShader~~~はシミュレータに対応していないため描画されません" ); Self.logged = true }
+            #endif
+            
             super.init( storage:storage )
             status?.shaderIndex = BBShader.shared.getFuncIndex( name:shaderName )
         }
--- Sources/Lily_Stage/Playground/Plane/Accessors/Lily.Stage.Playground.PGShaderActor.swift
+++ Sources/Lily_Stage/Playground/Plane/Accessors/Lily.Stage.Playground.PGShaderActor.swift
@@ -15,7 +15,13 @@ extension Lily.Stage.Playground.Plane
 {   
     open class PGShaderActor : PGActor
     {        
+        static private(set) var logged = false
+        
         public init( storage:PlaneStorage?, shaderName:String ) {
+            #if targetEnvironment(simulator)
+            if Self.logged == false { LLLog( "PGShader~~~はシミュレータに対応していないため描画されません" ); Self.logged = true }
+            #endif
+                        
             super.init( storage:storage )
             status?.shaderIndex = PGShader.shared.getFuncIndex( name:shaderName )
         }
